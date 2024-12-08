116ed554954ea50f18096f93b3197e95ee50388f
harshakallem22 <harshavardhanreddy.k@northeastern.edu>
2024-12-08 00:05:56 -0500

    added colour, changed font, table, button,textfields, label alignment

--- QuickCart/QuickCart/src/ui/Restaurant/RestaurantManager/ViewEmployeeJPanel.java
+++ QuickCart/QuickCart/src/ui/Restaurant/RestaurantManager/ViewEmployeeJPanel.java
@@ -30,6 +30,9 @@ public class ViewEmployeeJPanel extends javax.swing.JPanel {
         tblEmployees = new javax.swing.JTable();
         btnDelete = new javax.swing.JButton();
 
+        setBackground(new java.awt.Color(255, 62, 70));
+
+        tblEmployees.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
         tblEmployees.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null},
@@ -51,6 +54,7 @@ public class ViewEmployeeJPanel extends javax.swing.JPanel {
         });
         jScrollPane1.setViewportView(tblEmployees);
 
+        btnDelete.setFont(new java.awt.Font("Bahnschrift", 0, 15)); // NOI18N
         btnDelete.setText("Delete");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
